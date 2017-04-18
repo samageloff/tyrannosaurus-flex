789f2bb44400ac4e94781e4f59f5d0577d804cb0
Sam Ageloff <sam@getambassador.com>
2017-04-18 17:46:44 -0400

    housekeeping

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import './styles.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="flex-container">
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+        <div className="flex-item">
+          <div className="content" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
